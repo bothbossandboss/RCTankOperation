ce1f5c77de130a671a4cb20f7b2e05091d403e1f
bothbossandboss <bothboss21@gmail.com>
2015-03-14 01:45:03 +0900

    コードを整理

--- flask/hello_flask.py
+++ flask/hello_flask.py
@@ -1,4 +1,4 @@
-#update 2015-3-10
+#update 2015-3-11
 import os
 import time
 from flask import Flask, jsonify
@@ -7,20 +7,12 @@ import wiringpi
 #class : control motor
 from controller import Controller
 
-#PIN_L1 = 4
 debugId = "1"
 
 app = Flask(__name__)
 
-# set up
-#io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
-#io.pinMode(PIN_L1, io.PWM_OUTPUT)
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(22, GPIO.OUT)
-
 @app.route("/")
 def hello():
-#	io.pwmWrite(PIN_L1, 50)
 	return "Hello World!!!!!!"
 
 @app.route("/abc")
@@ -31,9 +23,6 @@ def abc():
 def ar():
 	os.system('gpio export 24 out')
 	os.system('gpio -g write 24 1')
-#	gpio = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
-#	gpio.pinMode(24, gpio.OUTPUT)
-#	gpio.digitalWrite(24, gpio.HIGH)
 	return "AR Ohsugi Yasuhito"
 
 @app.route("/sample/<id>", methods=['GET', 'POST'])
@@ -50,5 +39,4 @@ def move(direction):
 	return result
 
 if __name__ == "__main__":
-	#app = create_app()
     app.run(host='192.168.10.220', port=80, debug=True)
