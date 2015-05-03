4964a3cb49b935491f82fe22869baa8599880d5d
Yuki Nishizawa <yuki.zawazawanishi@gmail.com>
2015-05-03 14:05:25 +0900

    RPIO.PWMを使ってみる

--- flask/pwm_control.py
+++ flask/pwm_control.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import wiringpi2
+from RPIO import PWM
 
 argvs = sys.argv
 
@@ -15,28 +15,13 @@ PIN_TO_PWM_R2 = int(argvs[6])
 PARAM_R1 = int(argvs[7]) #R1 speed
 PARAM_R2 = int(argvs[8]) #R2 speed
 
-wiringpi2.wiringPiSetupGpio()
+PWM.setup()
+PWM.init_channel(0)
+PWM.init_channel(1)
+PWM.init_channel(2)
+PWM.init_channel(3)
 
-wiringpi2.pinMode(PIN_TO_PWM_L1, OUTPUT)
-wiringpi2.pinMode(PIN_TO_PWM_L2, OUTPUT)
-wiringpi2.pinMode(PIN_TO_PWM_R1, OUTPUT)
-wiringpi2.pinMode(PIN_TO_PWM_R2, OUTPUT)
-
-wiringpi2.softPwmCreate(PIN_TO_PWM_L1, 0, 100) # Setup PWM using Pin, Initial Value and Range parameters
-wiringpi2.softPwmCreate(PIN_TO_PWM_L2, 0, 100) # Setup PWM using Pin, Initial Value and Range parameters
-wiringpi2.softPwmCreate(PIN_TO_PWM_R1, 0, 100) # Setup PWM using Pin, Initial Value and Range parameters
-wiringpi2.softPwmCreate(PIN_TO_PWM_R2, 0, 100) # Setup PWM using Pin, Initial Value and Range parameters
-
-count = 0
-while 1 :
-	wiringpi2.softPwmWrite(PIN_TO_PWM_L1,PARAM_L1) # Change PWM duty cycle
-	wiringpi2.delay(10)
-	wiringpi2.softPwmWrite(PIN_TO_PWM_L2,PARAM_L2) # Change PWM duty cycle
-	wiringpi2.delay(10)
-	wiringpi2.softPwmWrite(PIN_TO_PWM_R1,PARAM_R1) # Change PWM duty cycle
-	wiringpi2.delay(10)
-	wiringpi2.softPwmWrite(PIN_TO_PWM_R2,PARAM_R2) # Change PWM duty cycle
-	wiringpi2.delay(10)
-	count += 40
-	if count > 5000 :
-		sys.exit()
+PWM.add_channel_pulse(0, PIN_TO_PWM_L1, 0, PARAM_L1)
+PWM.add_channel_pulse(0, PIN_TO_PWM_L2, 0, PARAM_L2)
+PWM.add_channel_pulse(0, PIN_TO_PWM_R1, 0, PARAM_R1)
+PWM.add_channel_pulse(0, PIN_TO_PWM_R2, 0, PARAM_R2)
